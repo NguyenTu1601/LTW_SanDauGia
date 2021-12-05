cb2520645cfe65f82a626bebbdb115ad0dabcf8e
tú nguyễn <19110035@student.hcmute.edu.vn>
2021-12-05 20:55:37 +0700

    test

--- src/main/java/com/ute/sandaugia/controllers/SanPhamServlet.java
+++ src/main/java/com/ute/sandaugia/controllers/SanPhamServlet.java
@@ -1,27 +1,47 @@
 package com.ute.sandaugia.controllers;
 
+import com.ute.sandaugia.beans.danhmuc;
+import com.ute.sandaugia.beans.loai;
+import com.ute.sandaugia.models.danhmucModels;
+import com.ute.sandaugia.utils.ServletUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "SanPhamServlet", value = "/SanPhamServlet")
+@WebServlet(name = "SanPhamServlet", value = "/SanPham/*")
 public class SanPhamServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getPathInfo();
-        if (path == null || path.equals("/")) {
-            path = "Index";
-        }
         switch (path) {
-            case "Index" :
-//                ServletUtils.forward("/views/vwHome/Index.jsp",request,response);
+            case "/DanhMuc":
+                int danhmucID = Integer.parseInt(request.getParameter("id"));
+                List<loai> list = danhmucModels.findByDanhMucID(danhmucID);
+
+                //sdff
+                request.setAttribute("products", list);
+                ServletUtils.forward("/views/vwProduct/ByCat.jsp", request, response);
+                break;
+
+            case "/Loai":
+                int proID = Integer.parseInt(request.getParameter("id"));
+                //Product product = ProductModels.findById(proID);
+//                if (product==null) {
+//                    ServletUtils.redirect("/Home", request, response);
+//                } else {
+//                    //request.setAttribute("product", product);
+//                    ServletUtils.forward("/views/vwProduct/Detail.jsp", request, response);
+//                }
+
                 break;
 
             default:
-                //ServletUtils.forward("/views/404.jsp",request,response);
+                //ServletUtils.forward("/views/404.jsp", request, response);
                 break;
         }
     }
--- src/main/java/com/ute/sandaugia/models/danhmucModels.java
+++ src/main/java/com/ute/sandaugia/models/danhmucModels.java
@@ -1,6 +1,7 @@
 package com.ute.sandaugia.models;
 
 import com.ute.sandaugia.beans.danhmuc;
+import com.ute.sandaugia.beans.loai;
 import com.ute.sandaugia.utils.DbUtils;
 import org.sql2o.Connection;
 
@@ -12,6 +13,13 @@ public class danhmucModels {
 
         try (Connection con = DbUtils.getConnection()) {
             return con.createQuery(query).executeAndFetch(danhmuc.class);
+        }
+    }
+    public static List<loai> findByDanhMucID(int idDanhMuc) {
+        final String query = "select * from loai where idDanhMuc = :idDanhMuc";
+
+        try (Connection con = DbUtils.getConnection()) {
+            return con.createQuery(query).addParameter("idDanhMuc", idDanhMuc).executeAndFetch(loai.class);
         }
     }
 }
